79a168c650b35801379553c093e017c4807af8e9
Ricardo D. Quiroga - L2Radamanthys <l2radamanthys@gmail.com>
2010-12-26 22:32:18 -0300

    Agregados los modelos restante, aunque no estan validados

--- TurnGest/GestionTurnos/models.py
+++ TurnGest/GestionTurnos/models.py
@@ -4,41 +4,151 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 
-class Usuario(User):
+#choices
+SEXO_CHOICE = {
+    ('M','Masculino'),
+    ('F', 'Femenino'),
+    ('-','-----')
+}
+
+SOLICITUD_ESTADO = {
+    ('P','Pendiente'),
+    ('A','Aceptado'),
+    ('C','Cancelado'),
+}
+
+class Usuarios(User):
     """
-    Para el login y el manejo de secciones
+        Para el login y el manejo de secciones
     """
     dni = models.IntegerField('DNI')
-    sexo = models.CharField('Sexo', max_length=1, select_sexo=[('M','Masculino'), ('F', 'Femenino'), ('-','-----')])
-    categoria = models.IntegerField('Categoria', select_categoria=[(0,'Paciente'), (1, 'Medico'), (2,'Administrativo')])
-    cod_categoria = models.IntegerField('CodCategoria')
+    sexo = models.CharField('Sexo', max_length=1, choice=SEXO_CHOICE, default='-')
     telefono = models.CharField('Nro de telefono', max_length=20)
+    
+    #esta categoria sera utilizada solo en caso de q el tipo de usuario sea Medico
+    matricula = models.CharField('Matricula Medica')
+
+    #FK y PK
+    tipo = models.ForeignKey(TipoUsuario)
+    
 
     #renombrados para mayor comodidad
     nombre = first_name
     apellido = last_name
-    
+
     #datos para recordar
     #username, password
 
+
+    def nombre_completo(self):
+        return self.first_name + self.last_name
 
 
     def __str__(self):
         return self.username
 
-    class Admin:
-        pass
+
+class TipoUsuario(models.Model):
+    """
+        En ves de crear las clases Medico, Paciente, Administrativo
+        cree una clase categoria q le definira la clase al paciente
+    """
+    nombre = models.CharField('Nro de telefono', max_length=30)
 
 
-class Medico(models.Model):
-    pass
+#remplazadas por la clase categoria
+#class Medico(models.Model):
+#    pass
+#class Paciente(models.Model):
+#    pass
+#class Administrativo(models.Model):
+#    pass
 
 
-class Paciente(models.Model):
-    pass
+class Expecialidades(models.Model):
+    """
+        Clasificacion de las expecialidades de todos los medicos
+    """
+    nombre = models.CharField('Nombre')
+
+    
+    def __str__(self):
+        return self.nombre
 
 
-class Administrativo(models.Model):
-    pass
+class ExpecialidadesMedicos(models.Model):
+    #fk
+    codigo_medico = models.ForeignKey(Usuarios)
+    cod_expecialidad = models.ForeignKey(Expecialidades)
+
+
+#sin usar por el momento
+class Consultorios(models.Model):
+    nombre = models.CharField()
+
+
+    def __str__(self):
+        return self.nombre
+
+
+class HorarioAtencion(models.Model):
+    """
+        Definicion de Horarios de Atencion del Medico
+    """
+    dia = models.CharField()
+    hora_inicio = models.CharField()
+    hora_fin = models.CharField()
+    duracion_turno = models.CharField() #duracion en minutos
+    intervalo = models.CharField() #entretiempo entre turnos
+
+    #FK
+    cod_medico = models.ForeignKey(Usuarios)
+    cod_expecialidad = models.ForeignKey(Expecialidades)
+
+
+    def __str__(self):
+        f_cad = "[ %s | Desde: %s | Hasta: %s ]" %(str(self.dia), str(self.hora_inicio), str(self.hora_fin))
+        return f_cad
+
+
+class DiasAtencion(models.Model):
+    """
+        Horario de Atencion en un dia Particular
+    """
+    fecha = models.DateField()
+    hora_inicio = models.TimeField()
+    hora_fin = models.TimeField()
+    cant_turno = models.CharField() #total de turnos asignados
+
+    #opcionales todavia no definidos si se agregaran
+    duracion_turno = models.TimeField() #duracion en minutos
+    intervalo = models.TimeField() #entretiempo entre turnos
+
+    #fk
+    cod_medico = models.ForeignKey(Usuarios)
+
+
+class Turnos(models.Model):
+    fecha = models.DateField()
+    hora_inicio = models.TimeField()
+    hora_fin = models.TimeField()
+    comentarios = models.TextField()
+
+    #fk
+    cod_paciente = models.ForeignKey(Usuarios)
+    cod_medico = models.ForeignKey(Usuarios)
+
+
+class SolitudesTurnos(models.Model):
+    """
+        Solicitudes de Turnos q realizan los Pacientes
+    """
+    fecha_solicitud = models.DateField() #fecha en la q se solicito el turno
+    fecha_requerida = models.DateField()#fecha para la cual se solicito turno
+    estado = models.CharField(lechoice=SOLICITUD_ESTADO, max_length=1, default='P')
+    comentarios = models.TextField()
+
+    #fk
+    cod_paciente = models.ForeignKey(Usuarios)
+    cod_medico = models.ForeignKey(Usuarios)
