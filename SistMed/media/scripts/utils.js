aa89c39fc66d232b5b24961c000170226712c254
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2011-03-13 23:41:18 -0300

    cambiado el estilo de los menu y adaptados con js para q sean desplegables

---
+++ SistMed/media/scripts/utils.js
@@ -0,0 +1,20 @@
+function desplegar(key) {
+    /*
+     Muestra/Oculta el menu dependiendo del estado anterior
+     */
+    var menu = document.getElementById(key);
+    if (menu.className == 'nav_menu') {
+        menu.className = 'hide_nav_menu';
+    } else {
+        menu.className = 'nav_menu';
+    }
+}
+
+function ocultar(key) {
+    /*
+     Oculta el menu
+    */
+    var menu = document.getElementById(key);
+    menu.className = 'hide_nav_menu';
+}
+
