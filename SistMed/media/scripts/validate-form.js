e25660461517211cec371ccf39a82a3afbf93a6c
L2Radamanthys <L2Radamanthys@gmail.com>
2011-09-27 00:17:32 -0300

    comenzando la creacion de los script de validacion de formulario y la parte de gestion de consultas..

---
+++ SistMed/media/scripts/validate-form.js
@@ -0,0 +1,19 @@
+
+
+function validate_form(forn_name, field) {
+
+}
+
+
+function validate_field(field_name, argv) {
+    /*
+     * 
+     */
+    field = document.getElementsByName(field_name);
+    alert(field.value);
+}
+
+
+function valid_lenght(field, argv) {
+    
+}
